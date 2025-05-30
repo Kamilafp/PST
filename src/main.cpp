e3e449960a70656fb6eba81d8c721498310478fb
Kamilafp <kamilafp25@gmail.com>
2025-05-30 16:12:20 +0700

    udan

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,46 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo myservo;
+#define analogPin A0
+#define digitalPin 4
+
+// Deklarasi 2 servo
+Servo servo1;
+Servo servo2;
 
 void setup() {
-  myservo.attach(D1);
+  Serial.begin(9600);
+  pinMode(digitalPin, INPUT);
+  // Kalibrasi servo dengan rentang mikrodetik (500-2500µs)
+  servo1.attach(D1, 500, 2500);  // Servo 1 di pin D1
+  servo2.attach(D3, 500, 2500);  // Servo 2 di pin D2
+  
+  // Inisialisasi posisi tengah (opsional)
+  servo1.writeMicroseconds(1500);  // 90°
+  servo2.writeMicroseconds(1500);  // 90°
+  delay(1000);
 }
 
 void loop() {
-  myservo.write(90);
+   int analogValue = analogRead(analogPin);
+
+  Serial.print("Analog Value: ");
+  Serial.print(analogValue);
+
+  if (analogValue < 500) {
+    Serial.println(" => RAIN DETECTED");
+  } else {
+    Serial.println(" => NO RAIN");
+  }
+
+  delay(500);
+  // Gerakan servo1: 0° → 180°
+  servo1.writeMicroseconds(500);    // 0°
+  servo2.writeMicroseconds(2500);   // Servo 2 tetap 180°
   delay(2000);
-  myservo.write(0);
+  
+  // Gerakan servo2: 180° → 0°
+  servo1.writeMicroseconds(2500);   // Servo 1 tetap 180°
+  servo2.writeMicroseconds(500);    // 0°
   delay(2000);
-  myservo.write(90);
-  delay(2000);
-  // for (int pos = 0; pos <= 180; pos++)
-  // {
-  //   myservo.write(pos);
-  //   delay(10);
-  // }
-  // for (int pos = 180; pos >= 0; pos--)
-  // {
-  //   myservo.write(pos);
-  //   delay(10);
-  // }
 }
